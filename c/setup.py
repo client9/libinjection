b8b24ed96c35e5e43522d0ef960c40b0c1049506
Nick Galbreath <nickg@client9.com>
2013-05-13 05:19:47 -0700

    cleanup

--- c/setup.py
+++ c/setup.py
@@ -1,16 +1,13 @@
-#
-# ALPHA
-# libinjection module for python
-#
-#  Copyright 2012, 2013 Nick Galbreath
-#  nickg@client9.com
-#  BSD License -- see COPYING.txt for details
-#
-#
+"""
+libinjection module for pytho
+ Copyright 2012, 2013 Nick Galbreath
+ nickg@client9.com
+ BSD License -- see COPYING.txt for details
+"""
 
 from distutils.core import setup, Extension
 
-module1 = Extension(
+MODULE = Extension(
     'libinjection',
     define_macros = [],
     include_dirs = [],
@@ -26,7 +23,7 @@ setup (
     author           = 'Nick Galbreath',
     author_email     = 'nickg@client9.com',
     url              = 'http://client9.com/libinjection',
-    ext_modules      = [module1],
+    ext_modules      = [MODULE],
     long_description = '''
 wrapper around libinjection
 ''',
