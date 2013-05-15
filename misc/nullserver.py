0beca37ea7495170063e9c4ede6728d3b36147dc
Nick Galbreath <nickg@client9.com>
2013-05-15 14:39:50 -0700

    cleanup automated check of sqlmap

--- misc/nullserver.py
+++ misc/nullserver.py
@@ -6,9 +6,12 @@
 
 import sys
 import logging
+import urllib
+
 import tornado.httpserver
 import tornado.ioloop
 import tornado.web
+import libinjection
 
 class ShutdownHandler(tornado.web.RequestHandler):
     def get(self):
@@ -16,43 +19,88 @@ class ShutdownHandler(tornado.web.RequestHandler):
         fd.close()
         sys.exit(0)
 
+
+class CountHandler(tornado.web.RequestHandler):
+    def get(self):
+        global count
+        self.write(str(count) + "\n")
+
+def boring(arg):
+    if arg == '':
+        return True
+
+    if arg == 'foo':
+        return True
+
+    if arg == 'NULL':
+        return True
+
+    try:
+        float(arg)
+        return True
+    except ValueError:
+        pass
+
+    return False;
+
 class NullHandler(tornado.web.RequestHandler):
 
     def get(self):
         global fd
-        param = self.request.query[3:]
+        global count
+        params = self.request.arguments.get('id', [])
+        sqli = False
 
-        isint = False
-        try:
-            float(param)
-            isint= True
-        except ValueError:
-            pass
+        if len(params) == 0 or (len(params) == 1 and boring(params[0])):
+            # if no args, or a single value with uninteresting input
+            # then just exit
+            self.write("<html><head><title>safe</title></head><body></body></html>")
+            return
 
-        token = 'admin'
-        if not isint and param != 'foo':
-            token = ''
-            fd.write(param + "\n")
-            fd.flush()
-        self.write("<html><head><title>ok</title></head><body>%s</body></html>" % (token,))
+        for arg in params:
+            sqli = libinjection.detectsqli(arg)
+            if sqli:
+                break
+
+        # we didn't detect it :-(
+        if not sqli:
+            count += 1
+            args = [ arg.strip() for arg in params ]
+            #fd.write(' | '.join(args) + "\n")
+            for arg in args:
+                extra = {}
+                sqli = libinjection.detectsqli(arg, extra)
+                logging.error("\t" + arg + "\t" + str(sqli) + "\t" + extra['fingerprint'] + "\n")
+            #for arg in param:
+            #    fd.write(arg + "\n")
+            #    #fd.write(urllib.quote_plus(arg) + "\n")
+            self.set_status(500)
+            self.write("<html><head><title>safe</title></head><body></body></html>")
+        else:
+            self.write("<html><head><title>sqli</title></head><body></body></html>")
 
 import os
 settings = {
     "static_path": os.path.join(os.path.dirname(__file__), "static"),
     "cookie_secret": "yo mama sayz=",
     "xsrf_cookies": True,
-    "gzip": True
+    "gzip": False
 }
 
 application = tornado.web.Application([
     (r"/null", NullHandler),
     (r"/shutdown", ShutdownHandler),
+    (r"/count", CountHandler)
     ], **settings)
 
 
 if __name__ == "__main__":
     global fd
-    fd = open('/tmp/urls.txt', 'w')
+    global count
+
+    count = 0
+
+    fd = open('./sqlmap-false-negatives.txt', 'w')
 
     import tornado.options
     #tornado.options.parse_config_file("/etc/server.conf")
--- misc/run_sqlmap.sh
+++ misc/run_sqlmap.sh
@@ -4,7 +4,7 @@
 # Then runs sqlmap
 #
 
-./nullserver.py &
+./nullserver.py --logging=none &
 
 if [ ! -d "sqlmap" ]; then
     git clone https://github.com/sqlmapproject/sqlmap.git
@@ -14,9 +14,16 @@ fi
 
 SQLMAP=./sqlmap/sqlmap.py
 URL=http://127.0.0.1:8888
-${SQLMAP} -v 0 -p id --level=5 --risk=3 --url=${URL}/null?id=1
-${SQLMAP} -v 0 -p id --level=5 --risk=3 --url=${URL}/null?id=1234.5
-${SQLMAP} -v 0 -p id --level=5 --risk=3 --url=${URL}/null?id=foo
+
+HPP=
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=1
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=1234.5
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=foo
+
+HPP=--hpp
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=1
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=1234.5
+${SQLMAP} ${HPP} -v 0 --titles -p id --level=5 --risk=3 --url=${URL}/null?id=foo
 
 curl -o /dev/null ${URL}/shutdown
 
