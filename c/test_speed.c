ca184f60d6f35ddd0b63907571e26a7eefa4df7f
Nick Galbreath <nickg@client9.com>
2013-05-15 14:21:09 -0700

    remove use of private apis from speed test

--- c/test_speed.c
+++ c/test_speed.c
@@ -4,72 +4,7 @@
 #include <string.h>
 #include <stdio.h>
 
-#include "libinjection_private.h"
-
-
-void testParseToken(void)
-{
-    const char *s =
-        "123 LIKE -1234.5678E+2; APPLE 19.123 'FOO' \"BAR\" /* BAR */ UNION ALL SELECT (2,3,4) || COS(+0X04) --FOOBAR";
-
-    const size_t slen = strlen(s);
-    const int imax = 1000000;
-    int i;
-    sfilter sf;
-    clock_t t0 = clock();
-    for (i = imax; i != 0; --i) {
-        sfilter_reset(&sf, s, slen);
-        while (parse_token(&sf)) {
-            //printf("[ %c, %s]\n", st.type, st.val);
-        }
-    }
-    clock_t t1 = clock();
-    double total = (double) (t1 - t0) / (double) CLOCKS_PER_SEC;
-    printf("Raw Tokenization TPS          = %f\n", (double) imax / total);
-}
-
-
-void testParseSyntax(void)
-{
-    const char *s =
-        "123 LIKE -1234.5678E+2; APPLE 19.123 'FOO' \"BAR\" /* BAR */ UNION ALL SELECT (2,3,4) || COS(+0X04) --FOOBAR";
-    const size_t slen = strlen(s);
-    const int imax = 1000000;
-    int i;
-    stoken_t st;
-    sfilter sf;
-    clock_t t0 = clock();
-    for (i = imax; i != 0; --i) {
-        sfilter_reset(&sf, s, slen);
-        while (sqli_tokenize(&sf, &st)) {
-            /* NOP */
-        }
-    }
-    clock_t t1 = clock();
-    double total = (double) (t1 - t0) / (double) CLOCKS_PER_SEC;
-    printf("SQLi Tokenize TPS             = %f\n", (double) imax / total);
-}
-
-void testParseFold(void)
-{
-    const char *s =
-        "123 LIKE -1234.5678E+2; APPLE 19.123 'FOO' \"BAR\" /* BAR */ UNION ALL SELECT (2,3,4) || COS(+0X04) --FOOBAR";
-    const size_t slen = strlen(s);
-    const int imax = 1000000;
-    int i;
-    stoken_t st;
-    sfilter sf;
-    clock_t t0 = clock();
-    for (i = imax; i != 0; --i) {
-        sfilter_reset(&sf, s, slen);
-        while (filter_fold(&sf, &st)) {
-            /* NOP */
-        }
-    }
-    clock_t t1 = clock();
-    double total = (double) (t1 - t0) / (double) CLOCKS_PER_SEC;
-    printf("Tokenize + Folding TPS        = %f\n", (double) imax / total);
-}
+#include "libinjection.h"
 
 void testIsSQL(void)
 {
@@ -90,9 +25,6 @@ void testIsSQL(void)
 
 int main()
 {
-    testParseToken();
-    testParseSyntax();
-    testParseFold();
     testIsSQL();
     return 0;
 }
