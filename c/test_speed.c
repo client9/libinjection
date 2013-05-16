df955528defe0e08d6d66f18aae9dbdb0a81a22b
Nick Galbreath <nickg@client9.com>
2013-05-15 17:12:17 -0700

    add a slightly better speed test

--- c/test_speed.c
+++ c/test_speed.c
@@ -8,15 +8,25 @@
 
 void testIsSQL(void)
 {
-    const char *s =
-        "123 LIKE -1234.5678E+2; APPLE 19.123 'FOO' \"BAR\" /* BAR */ UNION ALL SELECT (2,3,4) || COS(+0X04) --FOOBAR";
-    const size_t slen = strlen(s);
+    const char* s[] = {
+        "123 LIKE -1234.5678E+2;",
+        "APPLE 19.123 'FOO' \"BAR\"",
+        "/* BAR */ UNION ALL SELECT (2,3,4)",
+        "1 || COS(+0X04) --FOOBAR",
+        "dog apple cat banana bar",
+        "dog apple cat \"banana \'bar",
+        "102 TABLE CLOTH",
+        NULL
+    };
     const int imax = 1000000;
-    int i;
+    int i, j;
     sfilter sf;
     clock_t t0 = clock();
-    for (i = imax; i != 0; --i) {
-        libinjection_is_sqli(&sf, s, slen, NULL, NULL);
+    for (i = imax, j=0; i != 0; --i, ++j) {
+        if (s[j] == NULL) {
+            j = 0;
+        }
+        libinjection_is_sqli(&sf, s[j], strlen(s[j]), NULL, NULL);
     }
     clock_t t1 = clock();
     double total = (double) (t1 - t0) / (double) CLOCKS_PER_SEC;
