23fd3a4a84db72870023ff9906a8ce5a99dc3c6f
Nick Galbreath <nickg@client9.com>
2013-05-21 10:36:52 +0900

    bump to version 2.0.1

--- c/setup.py
+++ c/setup.py
@@ -19,7 +19,7 @@ MODULE = Extension(
 
 setup (
     name             = 'libinjection',
-    version          = '2.0.0',
+    version          = '2.0.1',
     description      = 'Wrapper around libinjection c-code to detect sqli',
     author           = 'Nick Galbreath',
     author_email     = 'nickg@client9.com',
