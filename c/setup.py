19c7a56dda5c205425f7acd2b910474c69818014
Nick Galbreath <nickg@client9.com>
2013-05-29 09:02:04 +0400

    pre5

--- c/setup.py
+++ c/setup.py
@@ -19,7 +19,7 @@ MODULE = Extension(
 
 setup (
     name             = 'libinjection',
-    version          = '3.0.0-pre4',
+    version          = '3.0.0-pre5',
     description      = 'Wrapper around libinjection c-code to detect sqli',
     author           = 'Nick Galbreath',
     author_email     = 'nickg@client9.com',
