663a33514742a469b5335ae8583132456d84604f
Nick Galbreath <nickg@client9.com>
2013-06-03 17:04:42 +0900

    simply build process

--- lua/generate_lua_fingerprints.py
+++ lua/generate_lua_fingerprints.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
 
+"""
+Generates a Lua table of fingerprints.
+One can then add, turn off or delete fingerprints from lua.
+"""
+
 def make_lua_table(obj):
+    """
+    Generates table.  Fingerprints don't contain any special chars
+    so they don't need to be escaped.  The output may be
+    sorted but it is not required.
+    """
     fp = obj[u'fingerprints']
     print("sqlifingerprints = {")
     for f in fp:
@@ -11,4 +21,6 @@ def make_lua_table(obj):
 if __name__ == '__main__':
     import sys
     import json
-    sys.exit(make_lua_table(json.load(sys.stdin)))
+    with open('../c/sqlparse_data.json', 'r') as fd:
+        make_lua_table(json.load(fd))
+
