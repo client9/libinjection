6ea0cc595a4d540adb6c78d82409e681e1324c3e
Nick Galbreath <nickg@client9.com>
2013-06-05 12:30:45 +0900

    update diagnostics page, will make pretty later.. riiiiiight

--- misc/sqliserver.py
+++ misc/sqliserver.py
@@ -42,6 +42,38 @@ class NullHandler(tornado.web.RequestHandler):
 
         sqlstate = libinjection.sfilter()
 
+        allfp = {}
+        for name,values in self.request.arguments.iteritems():
+            fps = []
+
+            val = values[0]
+            val = urllib.unquote(val)
+
+            pat = libinjection.sqli_fingerprint(sqlstate, val, libinjection.CHAR_NULL, libinjection.COMMENTS_ANSI)
+            issqli = bool(libinjection.sqli_blacklist(sqlstate))
+            fps.append(['unquoted', 'ansi', issqli, pat])
+
+            pat = libinjection.sqli_fingerprint(sqlstate, val, libinjection.CHAR_NULL, libinjection.COMMENTS_MYSQL)
+            issqli = bool(libinjection.sqli_blacklist(sqlstate))
+            fps.append(['unquoted', 'mysql', issqli, pat])
+
+            pat =libinjection.sqli_fingerprint(sqlstate, val, libinjection.CHAR_SINGLE, libinjection.COMMENTS_ANSI)
+            issqli = bool(libinjection.sqli_blacklist(sqlstate))
+            fps.append(['single', 'ansi', issqli, pat])
+
+            pat = libinjection.sqli_fingerprint(sqlstate, val, libinjection.CHAR_SINGLE, libinjection.COMMENTS_MYSQL)
+            issqli = bool(libinjection.sqli_blacklist(sqlstate))
+            fps.append(['single', 'mysql', issqli, pat])
+
+            pat = libinjection.sqli_fingerprint(sqlstate, val, libinjection.CHAR_DOUBLE, libinjection.COMMENTS_MYSQL)
+            issqli = bool(libinjection.sqli_blacklist(sqlstate))
+            fps.append(['double', 'mysql', issqli, pat])
+
+            allfp[name] = {
+                'value': val,
+                'fingerprints': fps
+            }
+
         for name,values in self.request.arguments.iteritems():
             for val in values:
                 # do it one more time include cut-n-paste was already url-encoded
@@ -53,13 +85,18 @@ class NullHandler(tornado.web.RequestHandler):
                 # True if any issqli values are true
                 qssqli = qssqli or issqli
                 val = val.replace(',', ', ')
-                args.append([name, val, issqli, sqlstate.pat])
+
+                pat = sqlstate.pat
+                if not issqli:
+                    pat = 'see below'
+                args.append([name, val, issqli, pat])
 
         self.render("form.html",
                     title='libjection sqli diagnositc',
                     version = libinjection.LIBINJECTION_VERSION,
                     is_sqli=qssqli,
                     args=args,
+                    allfp = allfp,
                     formvalue=formvalue
                     )
 
