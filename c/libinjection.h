60d84374687a3ae5b847d8044e75e3f81ebfdc12
Nick Galbreath <nickg@client9.com>
2013-06-07 13:11:09 +0900

    3.0.0pre10

--- c/libinjection.h
+++ c/libinjection.h
@@ -37,7 +37,7 @@ extern "C" {
  * See python's normalized version
  * http://www.python.org/dev/peps/pep-0386/#normalizedversion
  */
-#define LIBINJECTION_VERSION "3.0.0-pre9"
+#define LIBINJECTION_VERSION "3.0.0-pre10"
 
 #define ST_MAX_SIZE 32
 #define MAX_TOKENS 5
