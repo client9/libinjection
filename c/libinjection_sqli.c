b3104831d4dfdad7758b4e2254f6827dec68d4f7
Nick Galbreath <nickg@client9.com>
2013-06-13 14:45:22 +0900

    fix parsing of $.foo

--- c/libinjection_sqli.c
+++ c/libinjection_sqli.c
@@ -1042,9 +1042,8 @@ static size_t parse_money(sfilter *sf)
             }
         }
     } else if (xlen == 1 && cs[pos + 1] == '.') {
-        /* $. should parsed as '$' + '.' .. */
-        st_assign_char(sf->current, TYPE_BAREWORD, pos, 1, '$');
-        return pos + 1;
+        /* $. should parsed as a word */
+        return parse_word(sf);
     } else {
         st_assign(sf->current, TYPE_NUMBER, pos, 1 + xlen, cs + pos);
         return pos + 1 + xlen;
