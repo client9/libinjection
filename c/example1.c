eb9d97d582367f7be2461dbdde93b946bcc753b6
Nick Galbreath <nickg@client9.com>
2013-06-28 10:22:51 +0900

    example code

---
+++ c/example1.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <strings.h>
+#include "libinjection.h"
+
+int main(int argc, const char* argv[])
+{
+    const char* input;
+    size_t slen;
+    struct libinjection_sqli_state state;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s inputstring\n", argv[0]);
+        return -1;
+    }
+
+    input = argv[1];
+    slen = strlen(input);
+
+    libinjection_sqli_init(&state, input, slen, FLAG_NONE);
+    int issqli = libinjection_is_sqli(&state);
+    if (issqli) {
+        printf("sqli with fingerprint of '%s'\n", state.fingerprint);
+    } else {
+        printf("not sqli\n");
+    }
+    return issqli;
+}
