e4390dba016a80da4473c9229899294eece18b72
Nick Galbreath <nickg@client9.com>
2013-08-30 01:55:55 +0900

    closer to working php tests

--- php/testdriver.php
+++ php/testdriver.php
@@ -21,22 +21,47 @@ function readtestdata($filename) {
     );
 }
 
+function fileproviders($pattern) {
+    $args = array();
+    foreach (glob($pattern) as $a) {
+        array_push($args, array($a));
+    }
+    return $args;
+}
+
 function runtest($testname, $flag, $sqli_flags) {
 
 }
 
-function test_tokens() {
+class LibinjectionTestTokens extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @dataProvider filesTokens
+     */
+    public function testTokens($a) {
+        True;
+    }
+    public function filesTokens() {
+        return fileproviders('../tests/test-tokens-*.txt');
+    }
 
-}
+    /**
+     * @dataProvider filesFolding
+     */
+    public function testFolding($a) {
+        return True;
+    }
+    public function filesFolding() {
+        return fileproviders('../tests/test-folding-*.txt');
+    }
 
-function test_tokens_mysql() {
-
-}
-
-function test_folding() {
-
-}
-
-function test_fingerprints() {
-
+    /**
+     * @dataProvider filesFingerprints
+     */
+    public function testFingerprints($a) {
+        return True;
+    }
+    public function filesFingerprints() {
+        return fileproviders('../tests/test-sqli-*.txt');
+    }
 }
