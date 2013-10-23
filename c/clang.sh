fc7ac81c24d9c60777145569b13ba4014eb29fbb
Nick Galbreath <nickg@client9.com>
2013-10-24 02:19:57 +0900

    try to make clang work

--- c/clang.sh
+++ c/clang.sh
@@ -3,6 +3,7 @@
 #
 # adjust as needed for your clang setup
 #
+make clean
 export CC=clang
 export CFLAGS="-g -O3 -Weverything -Wno-padded -L/usr/lib/gcc/x86_64-amazon-linux/4.7.2/"
-make -e test
+COMPILER_PATH=/usr/lib/gcc/x86_64-amazon-linux/4.7.2/ make -e test
