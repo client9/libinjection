aec305ca6e47ec76146b23b9b4b66bf3d26ea089
Nick Galbreath <nickg@client9.com>
2013-10-24 03:24:43 +0900

    rework static analyzer

---
+++ c/clang-static-analyzer.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+rm -f testdriver
+
+scan-build --status-bugs -no-failure-report \
+-enable-checker alpha.core.BoolAssignment \
+-enable-checker alpha.core.CastSize \
+-enable-checker alpha.core.CastToStruct \
+-enable-checker alpha.core.FixedAddr \
+-enable-checker alpha.core.PointerArithm \
+-enable-checker alpha.core.PointerSub \
+-enable-checker alpha.core.SizeofPtr \
+-enable-checker alpha.deadcode.IdempotentOperations \
+-enable-checker alpha.deadcode.UnreachableCode \
+-enable-checker alpha.security.ArrayBound \
+-enable-checker alpha.security.ArrayBoundV2 \
+-enable-checker alpha.security.MallocOverflow \
+-enable-checker alpha.security.ReturnPtrRange \
+-enable-checker alpha.unix.cstring.BufferOverlap \
+-enable-checker alpha.unix.cstring.OutOfBounds \
+-enable-checker security.FloatLoopCounter \
+-enable-checker security.insecureAPI.rand \
+-enable-checker security.insecureAPI.strcpy \
+make testdriver
