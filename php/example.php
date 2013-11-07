7d2b59881036c60938c5d4b3f7d5577337b54559
Nick Galbreath <nickg@client9.com>
2013-11-07 16:01:48 +0900

    fix sample code

--- php/example.php
+++ php/example.php
@@ -11,10 +11,9 @@ $x = new_libinjection_sqli_state();
 // pass it in to init
 // arg 1 -- state objection above
 // arg 2 -- php string of input -- MUST BE URL-DECODED
-// arg 3 -- the length of the string of input
-// arg 4 -- flags -- just pass in '0' for now
+// arg 3 -- flags -- just pass in '0' for now
 $input = "1 union select 1,2,3,4--";
-libinjection_sqli_init($x, $input, strlen($input), 0);
+libinjection_sqli_init($x, $input, 0);
 
 // do a test
 $sqli = libinjection_is_sqli($x);
