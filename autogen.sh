174fb2432b53673b68b1a27fa28774f6afdec85f
Nick Galbreath <nickg@client9.com>
2014-03-02 17:34:35 +0900

    autoconf 2.68/2.69 compatibility

--- autogen.sh
+++ autogen.sh
@@ -1,3 +1,8 @@
 #!/bin/sh
+
+# autoconf 2.68 requires the m4 directory to
+#  exist.  2.69 makes it for you.
+if [ ! -d m4 ]; then mkdir m4; fi
+
 autoreconf --install
 automake --add-missing >/dev/null 2>&1
