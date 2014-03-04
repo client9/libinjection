1083e91b059bfce7262193ef2889e327535a091e
Nick Galbreath <nickg@client9.com>
2014-03-04 11:51:44 +0900

    use debian best-practices for gcc hardening

--- configure-gcc-hardened.sh
+++ configure-gcc-hardened.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 #
-# adjust as needed for your clang setup
+# See https://wiki.debian.org/Hardening for details
 #
 # -Wno-padded padding can change by OS/version this check is really
 #   for embedded systems so it's ok to skip
@@ -11,6 +11,6 @@ set -e
 #  typed, it's good to have a default: assert(0) in case someone does
 #  a bad cast, etc also this conflicts with GCC checks.
 #
-export CFLAGS="-g -O3 -fPIC -Wall -Wextra -Werror -Wcast-align -Wshadow -Wpointer-arith -Wcast-qual -Wstack-protector -D_FORTIFY_SOURCE=2 -ansi -pedantic"
+export CFLAGS="-g -O3 -pie -fPIE -fPIC -fstack-protector --param ssp-buffer-size=4 -Wall -Wextra -Wformat -Wformat-security -Werror -Wcast-align -Wshadow -Wpointer-arith -Wcast-qual -Wstack-protector -D_FORTIFY_SOURCE=2 -ansi -pedantic"
 ./configure --with-pic
 make clean
