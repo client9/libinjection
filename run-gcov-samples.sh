f31665889891aff3ff88ae549120714ba490c9aa
Nick Galbreath <nickg@client9.com>
2014-04-13 00:05:45 +0900

    Attempt to remove test harness from output

--- run-gcov-samples.sh
+++ run-gcov-samples.sh
@@ -14,5 +14,5 @@ libtool --mode=execute ./reader -q ../data/sqli-*.txt
 libtool --mode=execute ./reader -q -x ../data/xss-*.txt
 lcov -b . --directory . --capture --output-file libinjection.info
 lcov -b . --directory . --remove libinjection.info '/usr/include*' -o libinjection.info
-lcov -b . --directory . --remove libinjection.info 'reader' -o libinjection.info
+lcov -b . --directory . --remove libinjection.info 'reader.c' -o libinjection.info
 genhtml --branch-coverage -o lcov-html libinjection.info
