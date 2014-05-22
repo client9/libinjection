1f3471f25883783f7cb1fcdb4b279aa84a9cd5bc
Nick Galbreath <nickg@client9.com>
2014-05-22 11:11:47 -0700

    whitespace

--- src/libinjection_xss.c
+++ src/libinjection_xss.c
@@ -30,109 +30,109 @@ typedef struct stringtype {
 
 
 static const int gsHexDecodeMap[256] = {
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  0,   1,   2,   3,   4,   5,   6,   7,   8,   9, 256, 256,
-  256, 256, 256, 256, 256,  10,  11,  12,  13,  14,  15, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256,  10,  11,  12,  13,  14,  15, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
-  256, 256, 256, 256
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    0,   1,   2,   3,   4,   5,   6,   7,   8,   9, 256, 256,
+    256, 256, 256, 256, 256,  10,  11,  12,  13,  14,  15, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256,  10,  11,  12,  13,  14,  15, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256, 256,
+    256, 256, 256, 256
 };
 
 static int html_decode_char_at(const char* src, size_t len, size_t* consumed)
 {
-  int val = 0;
-  size_t i;
-  int ch;
+    int val = 0;
+    size_t i;
+    int ch;
 
-  if (len == 0 || src == NULL) {
-    *consumed = 0;
-    return -1;
-  }
-
-  *consumed = 1;
-  if (*src != '&' || len < 2) {
-    return (unsigned char)(*src);
-  }
-
-
-  if (*(src+1) != '#') {
-    /* normally this would be for named entities
-     * but for this case we don't actually care
-     */
-    return '&';
-  }
-
-  if (*(src+2) == 'x' || *(src+2) == 'X') {
-    ch = (unsigned char) (*(src+3));
-    ch = gsHexDecodeMap[ch];
-    if (ch == 256) {
-      /* degenerate case  '&#[?]' */
-      return '&';
+    if (len == 0 || src == NULL) {
+        *consumed = 0;
+        return -1;
     }
-    val = ch;
-    i = 4;
-    while (i < len) {
-      ch = (unsigned char) src[i];
-      if (ch == ';') {
-	*consumed = i + 1;
-	return val;
-      }
-      ch = gsHexDecodeMap[ch];
-      if (ch == 256) {
-	*consumed = i;
-	return val;
-      }
-      val = (val * 16) + ch;
-      if (val > 0x1000FF) {
-	return '&';
-      }
-      ++i;
+
+    *consumed = 1;
+    if (*src != '&' || len < 2) {
+        return (unsigned char)(*src);
     }
-    *consumed = i;
-    return val;
-  } else {
-    i = 2;
-    ch = (unsigned char) src[i];
-    if (ch < '0' || ch > '9') {
-      return '&';
+
+
+    if (*(src+1) != '#') {
+        /* normally this would be for named entities
+         * but for this case we don't actually care
+         */
+        return '&';
     }
-    val = ch - '0';
-    i += 1;
-    while (i < len) {
-      ch = (unsigned char) src[i];
-      if (ch == ';') {
-	*consumed = i + 1;
-	return val;
-      }
-      if (ch < '0' || ch > '9') {
-	*consumed = i;
-	return val;
-      }
-      val = (val * 10) + (ch - '0');
-      if (val > 0x1000FF) {
-	return '&';
-      }
-      ++i;
+
+    if (*(src+2) == 'x' || *(src+2) == 'X') {
+        ch = (unsigned char) (*(src+3));
+        ch = gsHexDecodeMap[ch];
+        if (ch == 256) {
+            /* degenerate case  '&#[?]' */
+            return '&';
+        }
+        val = ch;
+        i = 4;
+        while (i < len) {
+            ch = (unsigned char) src[i];
+            if (ch == ';') {
+                *consumed = i + 1;
+                return val;
+            }
+            ch = gsHexDecodeMap[ch];
+            if (ch == 256) {
+                *consumed = i;
+                return val;
+            }
+            val = (val * 16) + ch;
+            if (val > 0x1000FF) {
+                return '&';
+            }
+            ++i;
+        }
+        *consumed = i;
+        return val;
+    } else {
+        i = 2;
+        ch = (unsigned char) src[i];
+        if (ch < '0' || ch > '9') {
+            return '&';
+        }
+        val = ch - '0';
+        i += 1;
+        while (i < len) {
+            ch = (unsigned char) src[i];
+            if (ch == ';') {
+                *consumed = i + 1;
+                return val;
+            }
+            if (ch < '0' || ch > '9') {
+                *consumed = i;
+                return val;
+            }
+            val = (val * 10) + (ch - '0');
+            if (val > 0x1000FF) {
+                return '&';
+            }
+            ++i;
+        }
+        *consumed = i;
+        return val;
     }
-    *consumed = i;
-    return val;
-  }
 }
 
 
@@ -169,17 +169,17 @@ static stringtype_t BLACKATTR[] = {
 /* xml-stylesheet > <eval>, <if expr=> */
 
 /*
-static const char* BLACKATTR[] = {
-    "ATTRIBUTENAME",
-    "BACKGROUND",
-    "DATAFORMATAS",
-    "HREF",
-    "SCROLL",
-    "SRC",
-    "STYLE",
-    "SRCDOC",
-    NULL
-};
+  static const char* BLACKATTR[] = {
+  "ATTRIBUTENAME",
+  "BACKGROUND",
+  "DATAFORMATAS",
+  "HREF",
+  "SCROLL",
+  "SRC",
+  "STYLE",
+  "SRCDOC",
+  NULL
+  };
 */
 
 static const char* BLACKTAG[] = {
@@ -213,36 +213,36 @@ static const char* BLACKTAG[] = {
 
 static int cstrcasecmp_with_null(const char *a, const char *b, size_t n)
 {
-  char ca;
-  char cb;
-  /* printf("Comparing to %s %.*s\n", a, (int)n, b); */
-  while (n-- > 0) {
-    cb = *b++;
-    if (cb == '\0') continue;
+    char ca;
+    char cb;
+    /* printf("Comparing to %s %.*s\n", a, (int)n, b); */
+    while (n-- > 0) {
+        cb = *b++;
+        if (cb == '\0') continue;
 
-    ca = *a++;
+        ca = *a++;
 
-    if (cb >= 'a' && cb <= 'z') {
-      cb -= 0x20;
+        if (cb >= 'a' && cb <= 'z') {
+            cb -= 0x20;
+        }
+        /* printf("Comparing %c vs %c with %d left\n", ca, cb, (int)n); */
+        if (ca != cb) {
+            return 1;
+        }
     }
-    /* printf("Comparing %c vs %c with %d left\n", ca, cb, (int)n); */
-    if (ca != cb) {
-      return 1;
-    }
-  }
 
-  if (*a == 0) {
-    /* printf(" MATCH \n"); */
-    return 0;
-  } else {
-    return 1;
-  }
+    if (*a == 0) {
+        /* printf(" MATCH \n"); */
+        return 0;
+    } else {
+        return 1;
+    }
 }
 
 /*
  * Does an HTML encoded  binary string (const char*, lenght) start with
  * a all uppercase c-string (null terminated), case insenstive!
- * 
+ *
  * also ignore any embedded nulls in the HTML string!
  *
  * return 1 if match / starts with
@@ -250,47 +250,47 @@ static int cstrcasecmp_with_null(const char *a, const char *b, size_t n)
  */
 static int htmlencode_startswith(const char *a, const char *b, size_t n)
 {
-  size_t consumed;
-  int cb;
-  int first = 1;
-  /* printf("Comparing %s with %.*s\n", a,(int)n,b); */
+    size_t consumed;
+    int cb;
+    int first = 1;
+    /* printf("Comparing %s with %.*s\n", a,(int)n,b); */
     while (n > 0) {
-      if (*a == 0) {
-	/* printf("Match EOL!\n"); */
-	return 1;
-      }
-      cb = html_decode_char_at(b, n, &consumed);
-      b += consumed;
-      n -= consumed;
+        if (*a == 0) {
+            /* printf("Match EOL!\n"); */
+            return 1;
+        }
+        cb = html_decode_char_at(b, n, &consumed);
+        b += consumed;
+        n -= consumed;
 
-      if (first && cb <= 32) {
-        /* ignore all leading whitespace and control characters */
-	continue;
-      }
-      first = 0;
+        if (first && cb <= 32) {
+            /* ignore all leading whitespace and control characters */
+            continue;
+        }
+        first = 0;
 
         if (cb == 0) {
-	  /* always ignore null characters in user input */
-	  continue;
-	}
+            /* always ignore null characters in user input */
+            continue;
+        }
 
         if (cb == 10) {
-	  /* always ignore vtab characters in user input */
-	  /* who allows this?? */
-	  continue;
-	}
+            /* always ignore vtab characters in user input */
+            /* who allows this?? */
+            continue;
+        }
 
         if (cb >= 'a' && cb <= 'z') {
-	  /* upcase */
+            /* upcase */
             cb -= 0x20;
         }
 
         if (*a != (char) cb) {
-	  /* printf("    %c != %c\n", *a, cb); */
-	  /* mismatch */
-	  return 0;
+            /* printf("    %c != %c\n", *a, cb); */
+            /* mismatch */
+            return 0;
         }
-	a++;
+        a++;
     }
 
     return (*a == 0) ? 1 : 0;
@@ -306,8 +306,8 @@ static int is_black_tag(const char* s, size_t len)
 
     black = BLACKTAG;
     while (*black != NULL) {
-      if (cstrcasecmp_with_null(*black, s, len) == 0) {
-	/* printf("Got black tag %s\n", *black); */
+        if (cstrcasecmp_with_null(*black, s, len) == 0) {
+            /* printf("Got black tag %s\n", *black); */
             return 1;
         }
         black += 1;
@@ -317,7 +317,7 @@ static int is_black_tag(const char* s, size_t len)
     if ((s[0] == 's' || s[0] == 'S') &&
         (s[1] == 'v' || s[1] == 'V') &&
         (s[2] == 'g' || s[2] == 'G')) {
-      /*	printf("Got SVG tag \n"); */
+        /*        printf("Got SVG tag \n"); */
         return 1;
     }
 
@@ -325,7 +325,7 @@ static int is_black_tag(const char* s, size_t len)
     if ((s[0] == 'x' || s[0] == 'X') &&
         (s[1] == 's' || s[1] == 'S') &&
         (s[2] == 'l' || s[2] == 'L')) {
-      /*      printf("Got XSL tag\n"); */
+        /*      printf("Got XSL tag\n"); */
         return 1;
     }
 
@@ -342,7 +342,7 @@ static attribute_t is_black_attr(const char* s, size_t len)
 
     /* javascript on.* */
     if ((s[0] == 'o' || s[0] == 'O') && (s[1] == 'n' || s[1] == 'N')) {
-      /* printf("Got javascript on- attribute name\n"); */
+        /* printf("Got javascript on- attribute name\n"); */
         return TYPE_BLACK;
     }
 
@@ -350,7 +350,7 @@ static attribute_t is_black_attr(const char* s, size_t len)
     if (len >= 5) {
         /* XMLNS can be used to create arbitrary tags */
         if (cstrcasecmp_with_null("XMLNS", s, 5) == 0 || cstrcasecmp_with_null("XLINK", s, 5) == 0) {
-	  /*	  printf("Got XMLNS and XLINK tags\n"); */
+            /*      printf("Got XMLNS and XLINK tags\n"); */
             return TYPE_BLACK;
         }
     }
@@ -358,7 +358,7 @@ static attribute_t is_black_attr(const char* s, size_t len)
     black = BLACKATTR;
     while (black->name != NULL) {
         if (cstrcasecmp_with_null(black->name, s, len) == 0) {
-	  /*	  printf("Got banned attribute name %s\n", black->name); */
+            /*      printf("Got banned attribute name %s\n", black->name); */
             return black->atype;
         }
         black += 1;
@@ -385,11 +385,11 @@ static int is_black_url(const char* s, size_t len)
          * HEY: this is a signed character.
          *  We are intentionally skipping high-bit characters too
          *  since they are not ascii, and Opera sometimes uses UTF8 whitespace.
-	 *
-	 * Also in EUC-JP some of the high bytes are just ignored.
+         *
+         * Also in EUC-JP some of the high bytes are just ignored.
          */
-      ++s;
-      --len;
+        ++s;
+        --len;
     }
 
     if (htmlencode_startswith(data_url, s, len)) {
@@ -440,9 +440,9 @@ int libinjection_is_xss(const char* s, size_t len, int flags)
              * handle escaped characters
              */
             /*
-            if (memchr(h5.token_start, '<', h5.token_len) != NULL) {
-                return 1;
-            }
+              if (memchr(h5.token_start, '<', h5.token_len) != NULL) {
+              return 1;
+              }
             */
 
             switch (attr) {
@@ -464,13 +464,13 @@ int libinjection_is_xss(const char* s, size_t len, int flags)
                 }
                 break;
 /*
-            default:
-                assert(0);
+  default:
+  assert(0);
 */
             }
             attr = TYPE_NONE;
         } else if (h5.token_type == TAG_COMMENT) {
-	    /* IE uses a "`" as a tag ending char */
+            /* IE uses a "`" as a tag ending char */
             if (memchr(h5.token_start, '`', h5.token_len) != NULL) {
                 return 1;
             }
@@ -490,7 +490,7 @@ int libinjection_is_xss(const char* s, size_t len, int flags)
             }
 
             if (h5.token_len > 5) {
-	        /*  IE <?import pseudo-tag */
+                /*  IE <?import pseudo-tag */
                 if (cstrcasecmp_with_null("IMPORT", h5.token_start, 6) == 0) {
                     return 1;
                 }
@@ -510,22 +510,22 @@ int libinjection_is_xss(const char* s, size_t len, int flags)
  * wrapper
  */
 int libinjection_xss(const char* s, size_t len)
-{ 
-  if (libinjection_is_xss(s, len, DATA_STATE)) {
-      return 1;
-  }
-  if (libinjection_is_xss(s, len, VALUE_NO_QUOTE)) {
-    return 1;
-  }
-  if (libinjection_is_xss(s, len, VALUE_SINGLE_QUOTE)) {
-    return 1;
-  }
-  if (libinjection_is_xss(s, len, VALUE_DOUBLE_QUOTE)) {
-    return 1;
-  }
-  if (libinjection_is_xss(s, len, VALUE_BACK_QUOTE)) {
-    return 1;
-  }
+{
+    if (libinjection_is_xss(s, len, DATA_STATE)) {
+        return 1;
+    }
+    if (libinjection_is_xss(s, len, VALUE_NO_QUOTE)) {
+        return 1;
+    }
+    if (libinjection_is_xss(s, len, VALUE_SINGLE_QUOTE)) {
+        return 1;
+    }
+    if (libinjection_is_xss(s, len, VALUE_DOUBLE_QUOTE)) {
+        return 1;
+    }
+    if (libinjection_is_xss(s, len, VALUE_BACK_QUOTE)) {
+        return 1;
+    }
 
-  return 0;
+    return 0;
 }
