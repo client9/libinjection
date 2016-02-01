5242ef167d98168a6430b9d9d9d17d8476e6ad47
Nick Galbreath <nickg@signalsciences.com>
2016-01-31 19:57:51 -0800

    spelling

--- src/clang.sh
+++ src/clang.sh
@@ -7,7 +7,7 @@
 #   for embedded systems so it's ok to skip
 #
 # -Wno-covered-switch-default Don't warn if we have a switch that
-#  covers all of an enum AND we have a default.  enums are only losely
+#  covers all of an enum AND we have a default.  enums are only loosely
 #  typed, it's good to have a default: assert(0) in case someone does
 #  a bad cast, etc also this conflicts with GCC checks.
 #
